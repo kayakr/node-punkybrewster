7c799dc4ef45bd5d24ddefb4daa4dad2877d5e60
Jonathan Hunt <git@huntdesign.co.nz>
2015-04-19 14:48:46 +1200

    Add regex for price.

--- index.js
+++ index.js
@@ -8,19 +8,25 @@ var beers = [];
 
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(body);
-
-    $('h2').slice(1).each(function(i, element) { // skip first h2.
-        beers[i] = {};
-        beers[i].name = $(this).text();
-        beers[i].price = $(this).next('.paragraph').text();
-        beers[i].abv = $(this).next('.paragraph').next('.paragraph').text();
-    });
-
-    //console.log(JSON.stringify(beers));
+    beers = parse(body);
     tabula(beers, {columns: ['name', 'price', 'abv'], sort: ['name']});
   }
   else {
-    echo url + ' returned ' + response.statusCode;
+    console.log(error);
+    echo(url + ' returned ' + response.statusCode);
   }
 });
+
+function parse(body) {
+  var beers = [];
+  var $ = cheerio.load(body);
+
+  $('h2').slice(1).each(function(i, element) { // skip first h2.
+      beers[i] = {};
+      beers[i].name = $(this).text();
+      beers[i].price = $(this).next('.paragraph').text().match(/^\$(\d+\.\d+)\/L$/)[1];
+      beers[i].abv = $(this).next('.paragraph').next('.paragraph').text();
+  });
+
+  return beers;
+}
